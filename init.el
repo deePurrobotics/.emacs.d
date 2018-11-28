4076bd41064bcc985999306a3f8510bfd3483621
linZHank <linzhank@gmail.com>
2018-11-27 23:15:23 -0500

    Update init.el

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
+       '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
 (when (not package-archive-contents)
@@ -29,16 +29,14 @@
 ;; --------------------------------------
 
 (setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'material t) ;; load material theme
+(load-theme 'atom-one-dark t) ;; load material theme
 (global-linum-mode t) ;; enable line numbers globally
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2) ;; set indent to 2 spaces
+(add-hook 'prog-mode-hook 'indent-guide-mode) ;; enable indent guide mode
 
 ;; PYTHON CONFIGURATION
 ;; --------------------------------------
 
 (elpy-enable)
-
 
 ;; use flycheck not flymake with elpy
 (when (require 'flycheck nil t)
@@ -49,23 +47,28 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
-;; AUCTEX CONFIGURATION
+;; END PYTHON CONFIG
 ;; --------------------------------------
 
-(setq TeX-auto-save t)
-(setq TeX-parse-self t)
-
-;; enable auto-fill-mode
-(add-hook 'LaTeX-mode-hook 'turn-on-auto-fill)
-;; enable folding
-(add-hook 'LaTeX-mode-hook (lambda ()
-                             (TeX-fold-mode 1)))
-
-; roslaunch and gazebo world highlighting
+;; XML CONFIGURATION
+;; --------------------------------------
+;; roslaunch highlighting
 (add-to-list 'auto-mode-alist '("\\.launch$" . xml-mode))
+;; gazebo world highlight
 (add-to-list 'auto-mode-alist '("\\.world$" . xml-mode))
-(add-to-list 'auto-mode-alist '("\\.xacro$" . xml-mode))
 
+;; END XML CONFIG
+;; --------------------------------------
+
+;; MARKDOWN CONFIGURATION
+;; --------------------------------------
+
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+
+;; END MARKDOWN CONFIG
+;; --------------------------------------
 
 ;; init.el ends here
 (custom-set-variables
@@ -73,12 +76,56 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   (vector "#ffffff" "#f36c60" "#8bc34a" "#fff59d" "#4dd0e1" "#b39ddb" "#81d4fa" "#263238"))
+ '(custom-enabled-themes (quote (atom-one-dark)))
+ '(custom-safe-themes
+   (quote
+    ("59171e7f5270c0f8c28721bb96ae56d35f38a0d86da35eab4001aebbd99271a8" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" default)))
+ '(fci-rule-color "#37474f")
+ '(hl-sexp-background-color "#1c1f26")
  '(package-selected-packages
    (quote
-    (yaml-mode auctex markdown-mode py-autopep8 material-theme flycheck elpy ein better-defaults))))
+    (indent-guide atom-one-dark-theme pyim python-switch-quotes auto-complete-auctex auctex py-autopep8 material-theme flycheck elpy ein better-defaults)))
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#f36c60")
+     (40 . "#ff9800")
+     (60 . "#fff59d")
+     (80 . "#8bc34a")
+     (100 . "#81d4fa")
+     (120 . "#4dd0e1")
+     (140 . "#b39ddb")
+     (160 . "#f36c60")
+     (180 . "#ff9800")
+     (200 . "#fff59d")
+     (220 . "#8bc34a")
+     (240 . "#81d4fa")
+     (260 . "#4dd0e1")
+     (280 . "#b39ddb")
+     (300 . "#f36c60")
+     (320 . "#ff9800")
+     (340 . "#fff59d")
+     (360 . "#8bc34a"))))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; AUCTeX CONFIGURATION
+;; --------------------------------------
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+;; pyim
+(require 'pyim)
+(require 'pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置
+(pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
+(setq default-input-method "pyim")
